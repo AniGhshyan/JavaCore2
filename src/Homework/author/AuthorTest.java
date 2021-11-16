69a5e8e316b03a6677a2d01538753c7f291acbe3
ani-gasparyan <ani-gasparyan-2021@mail.ru>
2021-11-16 10:35:02 +0400

    I have changed "AuthorTest" class.

--- src/Homework/author/AuthorTest.java
+++ src/Homework/author/AuthorTest.java
@@ -5,28 +5,23 @@ import java.util.Scanner;
 public class AuthorTest {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Author authors1 = new Author("Poxos", "Poxosyan", "Poxospoxos", 25, "boy");
-        Author authors2 = new Author("Petros", "Petrosyan", "Petrospetros", 26, "boy");
         AuthorStorage authorStorage = new AuthorStorage();
-        authorStorage.add(authors1);
-        authorStorage.add(authors2);
-        Author author3 = new Author();
-        author3.setName("Davit");
-        author3.setSurname("Davtyan");
-        author3.setAge(30);
-        author3.setEmail("davuk");
-        author3.setGendre("boy");
-        authorStorage.add(author3);
-        System.out.println("Քանի հեղինակ ունենք");
-        System.out.println(authorStorage.size);
-        int num = scanner.nextInt();
-        for (int i = 1; i <= 5; i++) {
-                if (num < 0 || num > authorStorage.size) {
-                    System.out.println("Այսպիսի հեղինակ չունենք");
-                    break;
-                }
-                else authorStorage.print();break;
+        for (int i = 0; i < 5; i++) {
+            System.out.println("name: ");
+            String name = scanner.next();
+            System.out.println("surname: ");
+            String surname = scanner.next();
+            System.out.println("email: ");
+            String email = scanner.next();
+            System.out.println("age: ");
+            int age = scanner.nextInt();
+            System.out.println("gemder: ");
+            String gender = scanner.next();
+            Author author1 = new Author(name, surname, email, age, gender);
+            authorStorage.add(author1);
         }
+        authorStorage.print();
+
     }
 }
 
